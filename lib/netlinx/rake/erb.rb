6a10fb774647c6bfbf58bf8db00f5ce55bb26084
Alex McLain <alex@alexmclain.com>
2015-01-30 15:12:04 -0800

    Moved compile task to netlinx-compile.

--- lib/netlinx/rake/erb.rb
+++ lib/netlinx/rake/erb.rb
@@ -1,19 +1,19 @@
 
 require 'yard'
+require 'netlinx/rake/compile'
 require 'netlinx/rake/src'
 
 require_relative 'erb/generate_erb'
 require_relative 'erb/generate_rpc'
-require_relative 'erb/compile'
 require_relative 'erb/lines'
 
 
 task :default=>[:compile, :doc, :pack]
 
+NetLinx::Rake::ERB::Lines.new
 NetLinx::Rake::ERB::GenerateERB.new
 NetLinx::Rake::ERB::GenerateRPC.new :generate_rpc => :generate_erb
-NetLinx::Rake::ERB::Compile.new :compile => :generate_rpc
-NetLinx::Rake::ERB::Lines.new
+NetLinx::Rake::Compile.new :compile => :generate_rpc
 
 # Generate Ruby documentation.
 YARD::Rake::YardocTask.new :doc do |t|
