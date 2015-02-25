c51e4816b1ff9e40a8a9e37b0f822f619da93203
Alex McLain <alex@alexmclain.com>
2015-02-25 11:21:17 -0800

    Updated spec_helper for coveralls and :focus.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,22 @@
+
 require 'pry'
 require 'rspec/its'
+require 'coveralls'
 
 require 'netlinx-erb'
+
+Coveralls.wear!
+
+Thread.abort_on_exception = true
 
 RSpec.configure do |c|
   # Enable 'should' syntax
   c.expect_with(:rspec) { |c| c.syntax = [:should, :expect] }
   c.mock_with(:rspec)   { |c| c.syntax = [:should, :expect] }
   
-  # Only run tests marked with iso:true.
-  c.filter_run_including iso:true
+  # Only run tests marked with focus:true.
+  # Enables use of fdescribe, fit, fspecify.
+  c.filter_run_including focus:true
   c.run_all_when_everything_filtered = true
   
   # Abort after first failure.
