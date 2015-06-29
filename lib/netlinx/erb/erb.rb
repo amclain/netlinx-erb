1feb67018f83c91fd6845a909a23d2f58643af76
Alex McLain <alex@alexmclain.com>
2015-06-29 16:01:10 -0700

    Refactored NetLinx::ERB.binding to accept a block for modifying the binding's parent object.

--- lib/netlinx/erb/erb.rb
+++ lib/netlinx/erb/erb.rb
@@ -5,31 +5,14 @@ module NetLinx
   module ERB
     
     # @return a binding for ERB to evaluate code in.
+    # @yieldparam [Module] module the object whose binding is returned.
     # @example
     #   ERB.new(buffer, nil, '%<>-').result(NetLinx::ERB.binding)
-    def self.binding
-      # Issue #2
-      # https://github.com/amclain/netlinx-erb/issues/2
-      # 
-      # Although ||= seemed like a good idea to pass data from _config.axi.erb
-      # to the templates, in reality it causes data to be retained between
-      # all files thoughout the entire generation process. This was causing
-      # _config.axi to be populated when it should have only contained the
-      # generation header.
-      # 
-      # This method needs to be refactored in a way that the module can be
-      # populated with general framework helpers, then captured by a project-
-      # specific configuration (_config.axi.erb) to be injected with project
-      # data, then have that module's binding passed back to
-      # NetLinx::ERB::Helpers.execute_erb. There needs to be a default binding
-      # for non-templated files.
-      
-      # @b ||= Module.new.instance_eval {
-      Module.new.instance_eval {
-        extend NetLinx::ERB::Helpers
-        # TODO: _config.rb instance variables should be injected here.
-        binding
-      }
+    def self.binding &block
+      Module.new.tap { |mod|
+        mod.instance_eval { extend NetLinx::ERB::Helpers }
+        block.call mod if block_given?
+      }.instance_eval { binding }
     end
     
     # Run ERB on the given template file.
@@ -48,8 +31,8 @@ module NetLinx
     #   k/v pairs that will be turned into `@tmpl_(key)` for use in the template.
     # @option kwargs [String] :template_dir ('./include/ui/template') directory
     #   containing `.axi.erb.tmpl` template files.
-    # @yieldparam [Module] module the object whose binding is used when evaluating the template.
-    #   This can be used to add customized data during evaluation.
+    # @yieldparam [Module] module the object whose binding is used when evaluating
+    #   the template. This can be used to add customized data during evaluation.
     # 
     # @example
     #   # Sample input for `devices` parameter:
@@ -62,7 +45,7 @@ module NetLinx
       template_files = Dir["#{template_dir}/*.axi.erb.tmpl"]
       
       devices.each do |name, params|
-        Module.new.tap do |m|
+        b = self.binding do |m|
           m.instance_eval {
             extend NetLinx::ERB::Helpers
             
@@ -82,19 +65,17 @@ module NetLinx
             Hash.instance_variable_set :@tmpl_var_suffix, @tmpl_var_suffix
           }
           
-          # TODO: Developer should be able to add instance variables in
-          #       the config file. (like @dvTP_ADMIN).
           block.call m if block_given?
-          
-          # Generate files.
-          template_files.each do |path|
-              file_base_name = File.basename(path).gsub(/\..*/, '')
-              output_dir = File.expand_path "#{template_dir}/.."
-              output_path = "#{output_dir}/#{file_base_name}-#{name.to_s.downcase.gsub('_', '-')}.axi"
-              
-              puts '   ' + output_path.gsub(Dir.pwd, '')[1..-1] # Print canonical path.
-              File.write output_path, execute(path, binding: m.instance_eval { binding })
-          end
+        end
+        
+        # Generate files.
+        template_files.each do |path|
+            file_base_name = File.basename(path).gsub(/\..*/, '')
+            output_dir = File.expand_path "#{template_dir}/.."
+            output_path = "#{output_dir}/#{file_base_name}-#{name.to_s.downcase.gsub('_', '-')}.axi"
+            
+            puts '   ' + output_path.gsub(Dir.pwd, '')[1..-1] # Print canonical path.
+            File.write output_path, execute(path, binding: b)
         end
       end
     end
